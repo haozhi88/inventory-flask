3f0a3a9fb3b89e370597372e4272608267f474e4
Lee <l783l74@hotmail.com>
2019-11-06 13:59:33 +0800

    add store input form

--- server.py
+++ server.py
@@ -6,20 +6,36 @@ app = Flask(__name__)
 
 @app.before_request
 def before_request():
-   db.connect()
+    db.connect()
 
 @app.after_request
 def after_request(response):
-   db.close()
-   return response
+    db.close()
+    return response
 
 @app.cli.command()
 def migrate(): 
-   db.evolve(ignore_tables={'base_model'})
+    db.evolve(ignore_tables={'base_model'})
 
 @app.route("/")
 def index():
-   return render_template('index.html')
+    return render_template('index.html')
+
+@app.route("/info")
+def info():
+    print("info")
+    return render_template('info.html')
+
+@app.route("/store", methods=["GET", "POST"])
+def store():
+    # name = request.args.get('store_name')
+    if request.method == "POST":
+        name = request.form.get('store_name')
+        print(f"New store name: {name}")
+
+    return render_template('store.html')
+
+
 
 if __name__ == '__main__':
-   app.run()
+    app.run()
