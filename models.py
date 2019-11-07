b6e839e7dada4422093dabe50986e5cc81552b8b
Lee <l783l74@hotmail.com>
2019-11-07 11:14:26 +0800

    refine validation and error message

--- models.py
+++ models.py
@@ -10,34 +10,42 @@ class BaseModel(pw.Model):
    updated_at = pw.DateTimeField(default=datetime.datetime.now)
 
    def save(self, *args, **kwargs):
-       self.updated_at = datetime.datetime.now()
-       return super(BaseModel, self).save(*args, **kwargs)
+      self.errors = []
+      self.validate()
+
+      if len(self.errors) == 0:
+         self.updated_at = datetime.datetime.now()
+         return super(BaseModel, self).save(*args, **kwargs)
+      else:
+         return 0
 
    class Meta:
-       database = db
-       legacy_table_names = False
+      database = db
+      legacy_table_names = False
 
 class Store(BaseModel):
    name = pw.CharField(unique=True)
 
-   def validate(name):
-      if len(name) > 0:
-         store = Store.get_or_none(Store.name == name)
-         if not store:
-            return True      
-      return False
+   def validate(self):
+      if len(self.name) > 0:
+         store = Store.get_or_none(Store.name == self.name)
+         if store:
+            self.errors.append("Store name not unique")
+      else:
+         self.errors.append("Store name cannot be empty")
 
 class Warehouse(BaseModel):
    store = pw.ForeignKeyField(Store, backref='warehouses', unique=True, on_delete="CASCADE")
    location = pw.TextField()
    # store_backup = pw.ForeignKeyField(Store, backref='warehouses', unique=True, on_delete="CASCADE", null=True)
 
-   def validate(store, location):
-      if len(location) > 0:
-         store = Store.get_or_none(Store.id == store.id)
-         if not store:
-            return True
-      return False
+   def validate(self):
+      if len(self.location) > 0:
+         stores = self.store.warehouses
+         if len(stores):
+            self.errors.append("This store has been selected")
+      else:
+         self.errors.append("Warehouse location cannot be empty")
 
 class Product(BaseModel):
    name = pw.CharField(index=True)
--- server.py
+++ server.py
@@ -6,6 +6,10 @@ from models import *
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
+def error_to_flash(errors):
+    for error in errors:
+        flash(error, 'alert alert-danger')
+
 @app.before_request
 def before_request():
     db.connect()
@@ -48,17 +52,13 @@ def new_store():
 def create_store():
     stores = Store.select()
     name = request.form.get('store_name')
-    if Store.validate(name):
-        store = Store(name=name)
-        if store.save():
-            flash('Create store successful', 'alert alert-primary')
-            return redirect(url_for('store'))
-        else:
-            flash('Create store fail', 'alert alert-danger')
-            return render_template('store.html', stores)
+    store = Store(name=name)
+    if store.save():
+        flash('Create store successful', 'alert alert-primary')
+        return redirect(url_for('store'))
     else:
-        flash('Store name is not appropriate', 'alert alert-danger')
-        return render_template('new_store.html')
+        error_to_flash(store.errors)
+        return render_template('new_store.html', errors=store.errors)
 
 @app.route("/store/<store_id>/delete", methods=["POST"])
 def delete_store(store_id):
@@ -86,7 +86,7 @@ def update_store(store_id):
         flash('Edit store successful', 'alert alert-primary')
         return redirect(url_for('store'))
     else:
-        flash('Edit store fail', 'alert alert-danger')
+        error_to_flash(store.errors)
         return render_template('edit_store.html', store_id=store.id)
 
 @app.route("/warehouse", methods=["GET", "POST"])
@@ -96,23 +96,14 @@ def warehouse():
         location = request.form.get('warehouse_location')
         store_id = request.form.get('store_id')
         store = Store.get_by_id(store_id)
-        if Warehouse.validate(store):
-            warehouse = Warehouse(location=location, store=store)
-            if warehouse.save():
-                flash('Create warehouse successful', 'alert alert-primary')
-                return redirect(url_for('index'))
-            else:
-                flash('Create warehouse fail', 'alert alert-danger')
-                return render_template('warehouse.html', stores=stores, warehouse=warehouse)
+        warehouse = Warehouse(location=location, store=store)
+        if warehouse.save():
+            flash('Create warehouse successful', 'alert alert-primary')
+            return redirect(url_for('index'))
         else:
-            flash('Cannot select this store or location is not appropriate', 'alert alert-danger')
-            return render_template('warehouse.html', stores=stores)
+            error_to_flash(warehouse.errors)
+            return render_template('warehouse.html', stores=stores, warehouse=warehouse)
     return render_template('warehouse.html', stores=stores)
 
 if __name__ == '__main__':
     app.run()
-
-# todo
-# 1. validate -> how to remain values? Why validate at save instead of creation?
-# 2. delete on cascade -> how to add this feature halfway?
-# 3. error -> how to show proper error?
